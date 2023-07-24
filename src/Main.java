4aedfd5c8c652e3aa4bdc681de2b4effe4b82d44
SONEKT <shulmanlevgen@gmail.com>
2023-07-24 21:18:24 +0500

    1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
+    public static void main(String[] args) {
+
+    }
+}
